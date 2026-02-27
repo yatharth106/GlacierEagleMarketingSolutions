ff202b082fc6a8541800a326fdd60586d20b0275
Wix Remote Machine <support@wix.com>
2026-02-27 08:27:32 +0000

    Fix 'Current Email Platform' field alignment

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-foreground/20 bg-transparent px-4 py-1 text-base text-foreground shadow-sm transition-colors hover:border-foreground/30 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-primary-foreground placeholder:text-foreground/50 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-border focus-visible:border-primary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex h-10 w-full rounded-md border border-foreground/20 bg-transparent px-4 py-2 text-base text-foreground shadow-sm transition-colors hover:border-foreground/30 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-primary-foreground placeholder:text-foreground/50 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-border focus-visible:border-primary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
